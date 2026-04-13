7c69caad651a481988944e0df56875164437a506
WhyATent <wwyatnt@gmail.com>
2026-04-13 13:08:04 -0700

    The program appears to be working, but the pytest is not. I've also edited data2.txt to include the correct value indicated by main_test.py.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@ using namespace std;
 int main()
 {
     int n;
+    char letter;
     // TODO: ask user for input
-
+    cout << "Input 1 value to determine number iterations.\n";
+    cin >> n;
     // TODO: outer loop for each row
-
+    for(int i=0; i<n; i++){
+        cout << endl;
+        for(int j=0; j<i+1; j++){
+            letter = 65+j;
+            cout << letter << " ";
+        }
+    }
+    cout << endl;
         // TODO: inner loop to print letters for this row
 
             // TODO: print the letter for this column
