af340bd36eb9f2e637b603f8c41246d68fb48a69
WhyATent <wwyatnt@gmail.com>
2026-04-13 13:46:45 -0700

    The program no longer prints a space at the last character. Changed Line 17, Added Lines 18 to 20. The if statement only puts a space between the letters if j<i, meaning that there will be no space after the last printed letter in the row because j=i at that point.

--- main.cpp
+++ main.cpp
@@ -6,22 +6,25 @@ int main()
     int n;
     char letter;
     // TODO: ask user for input
-    cout << "Input 1 value to determine number iterations.\n";
+    cout << "Input 1 value to determine number of iterations." << endl;
     cin >> n;
     // TODO: outer loop for each row
     for(int i=0; i<n; i++){
-        cout << endl;
+        // TODO: inner loop to print letters for this row
         for(int j=0; j<i+1; j++){
             letter = 65+j;
-            cout << letter << " ";
+            // TODO: print the letter for this column
+            cout << letter;
+            if (j<i){
+                cout << " ";
+            }
         }
+        // TODO: print newline after each row
+        cout << endl;
     }
     cout << endl;
-        // TODO: inner loop to print letters for this row
 
-            // TODO: print the letter for this column
 
-        // TODO: print newline after each row
 
 
     return 0;
